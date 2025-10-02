71bed4cfe42b35e4120836d842fdbbb9996ca798
LidorLevi123 <lidorlevi728@gmail.com>
2025-10-02 20:24:03 +0300

    change the header styling

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,23 @@
 import { NavLink } from 'react-router-dom'
 
 export function AppHeader() {
-
 	return (
 		<header className="app-header full">
-			<nav>
+			<nav className="header-nav container">
 				<NavLink to="/" className="logo">
-					E2E Demo
+					<span className="logo-text">Ad<span className="highlight">Stream</span></span>
 				</NavLink>
+				
+				<div className="nav-links">
+					<NavLink to="/" end>Home</NavLink>
+					<NavLink to="/discover">Discover</NavLink>
+					<NavLink to="/analytics">Analytics</NavLink>
+				</div>
+
+				<div className="header-actions">
+					<button className="btn-secondary">Sign In</button>
+					<button className="btn-primary">Get Started</button>
+				</div>
 			</nav>
 		</header>
 	)
