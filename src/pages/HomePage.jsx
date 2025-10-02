1da3a2984419b2c8ee43226e6e47e3ac3527ce46
LidorLevi123 <lidorlevi728@gmail.com>
2025-10-02 21:40:40 +0300

    clean code and use color vars

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import { httpService } from "../services/http.service"
 import { StatsDisplay } from "../cmps/StatsDisplay"
-import '../assets/styles/cmps/HomePage.css'
 
 export function HomePage() {
     const [domains, setDomains] = useState([])
